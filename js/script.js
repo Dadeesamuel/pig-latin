4fb614e5bc2f898f0e94237a64d0b6a7c28561c0
DADEE SAMUEL <dadeesamuel@gmail.com>
2021-11-29 12:50:23 +0100

    piglatin vowel

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-
+/*
 function translatePigLatin(str) {
     let vowels = ['a', 'e', 'i', 'o', 'u'];
     let newStr = "";
@@ -12,4 +12,23 @@ function translatePigLatin(str) {
         newStr = str.substring(vowel) + str.substring(0, vowel) + "ay";
         return newStr;
     }
-}
+} */
+//user logic
+$(document).ready(function(){
+    $("#form").submit(function(event){
+        event.preventDefault();
+    let userInput =$("#word").val();
+    let firstLetter= userInput.substring(0,1)
+    let remove  = userInput.slice(1)+firstLetter;
+    let vowels = ['a', 'e', 'i', 'o', 'u'];
+    let newStr = "";
+    if (vowels.indexOf(userInput[0]) > -1) {
+        newStr = userInput + "acedu";
+        console.log(newStr);  
+         
+    }
+
+   
+  //  $(".word").text(str)
+});
+});
